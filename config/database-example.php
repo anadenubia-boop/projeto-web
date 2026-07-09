bbb949535777cdf77c301293b5b0729af53992be
Denya <anadenubia@gmail.com>
2026-07-09 18:44:03 -0300

    arquivo exemplo de configuração para o PDO do banco de dados.

---
+++ config/database-example.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+
+    'driver' => 'mysql',
+
+    'host' => 'your_host',
+
+    'port' => 3306,
+
+    'database' => 'your_db_name',
+
+    'username' => 'your_user_name',
+
+    'password' => 'your_password',
+
+    'charset' => 'utf8mb4',
+
+];
