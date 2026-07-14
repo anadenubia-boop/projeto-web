bfef590a6b38646f7b11ee17deb5b9415dfcb249
Denya <anadenubia@gmail.com>
2026-07-13 22:42:42 -0300

    fix(catalog.controller): correção do caminho da intrução "use".

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-use Ana\FdsApp\Controller\HomeController;
+use Ana\FdsApp\Controller\Catalog\HomeController;
 use Ana\FdsApp\Controller\LoginController;
 
 /** @var \Ana\FdsApp\Http\Router $router */
