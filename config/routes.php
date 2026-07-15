c339bdcb3da3b7a79d8a39e0ad2f0733055aaae4
Denya <anadenubia@gmail.com>
2026-07-15 20:38:53 -0300

    feat(route.register): registra as rotas da aplicação.

--- config/routes.php
+++ config/routes.php
@@ -8,7 +8,3 @@ use Ana\FdsApp\Controller\LoginController;
 /** @var \Ana\FdsApp\Http\Router $router */
 
 $router->get('/', [HomeController::class, 'index']);
-
-$router->get('/login', [LoginController::class, 'index']);
-
-$router->post('/login', [LoginController::class, 'authenticate']);
