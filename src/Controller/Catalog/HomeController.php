0d40cde741b9e525a498eb5790768398d9bde6b4
Denya <anadenubia@gmail.com>
2026-07-16 14:07:46 -0300

    refactor(controller): refatoração de implementação do construtor e injeção do productService

--- src/Controller/Catalog/HomeController.php
+++ src/Controller/Catalog/HomeController.php
@@ -4,18 +4,24 @@ declare(strict_types=1);
 
 namespace Ana\FdsApp\Controller\Catalog;
 
-use Ana\FdsApp\Bootstrap\ProductBootstrap;
+use Ana\FdsApp\Domain\Product\Service\ProductService;
 use Ana\FdsApp\Controller\AbstractController;
 use Ana\FdsApp\Http\Request;
 use Ana\FdsApp\Http\Response;
 
+
 final class HomeController extends AbstractController
 {
+    
+    public function __construct(
+        private readonly ProductService $productService
+    ) {
+        parent::__construct();
+    }
+
     public function index(Request $request): Response
     {
-        $productService = ProductBootstrap::service();
-
-        $products = $productService->findAll();
+        $products = $this->productService->findAll();
 
         return $this->render(
             'catalog/index.html.twig',
