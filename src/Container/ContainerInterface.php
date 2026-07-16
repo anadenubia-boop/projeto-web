a3c9b7c46bffa086e723faefec5c87ea0eec9aec
Denya <anadenubia@gmail.com>
2026-07-16 12:17:24 -0300

    refactor(container.api): renomeação de 'set' -> 'register'

--- src/Container/ContainerInterface.php
+++ src/Container/ContainerInterface.php
@@ -6,7 +6,7 @@ namespace Ana\FdsApp\Container;
 
 interface ContainerInterface
 {
-    public function set(string $id, callable $factory): void;
+    public function register(string $id, callable $factory): void;
 
     public function get(string $id): object;
 
